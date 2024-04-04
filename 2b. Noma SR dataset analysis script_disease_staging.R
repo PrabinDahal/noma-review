1654754dacd2d2fddfe92a5c7ff42e756a0d8097
Prabin Dahal <prabin.dahal@wwarn.org>
2024-04-04 11:15:55 +0100

    Update 2b. Noma SR dataset analysis script_disease_staging.R

--- 2b. Noma SR dataset analysis script_disease_staging.R
+++ 2b. Noma SR dataset analysis script_disease_staging.R
@@ -1,8 +1,8 @@
 ################################################################################
-# Title			          :	Summarising Noma systematic literature review database
-# Data version		    :	March-2023
-# Analysis version		:	March-2023
-# Task                : Summarise the disease staging
+# Title                : Summarising Noma systematic literature review database
+# Data version         : March-2023
+# Analysis version     : March-2023
+# Task                 : Summarise the disease staging
 ################################################################################
 #rm(list=ls())
 library(pacman)
